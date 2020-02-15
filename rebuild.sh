5ade60b25a756a0100103b0730c7f5cf2c84fe53
LelouBil <bilel.medimegh@gmail.com>
2020-02-15 17:17:01 +0100

    Edit rebuild.sh to update packages

--- rebuild.sh
+++ rebuild.sh
@@ -5,3 +5,6 @@ rm -r docs
 java -jar swagger-codegen-cli-2.4.12.jar generate -i http://localhost:8080/v2/api-docs -l javascript -o . --artifact-version 1.0.0
 git add -A
 rm git_push.sh
+npm install
+npm audit fix --force
+npm test
