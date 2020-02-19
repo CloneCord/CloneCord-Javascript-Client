cd68c54fb204161e4fc04fa7646e0eb74f6c3f53
LelouBil <bilel.medimegh@gmail.com>
2020-02-19 16:49:10 +0100

    Update workflow file & script

--- rebuild.sh
+++ rebuild.sh
@@ -2,7 +2,7 @@
 rm -r src
 rm -r test
 rm -r docs
-echo "{\"projectVersion\": \"$1\" }" > config.json
+echo "{\"projectVersion\": \"$1\" }" >config.json
 java -jar openapi-generator-cli-4.2.3.jar generate -i http://localhost:8080/v3/api-docs -g javascript -c config.json -o .
 rm -r config.json
 git add -A
@@ -10,7 +10,7 @@ rm git_push.sh
 npm install
 npm audit fix
 npm test
-npm pack
 git commit -m "Bump version to $1"
-git tag "v$1"
 git push
+git tag "v$1"
+git push --tags
